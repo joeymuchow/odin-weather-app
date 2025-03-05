0ba6018477867c3171020f592abc06606acf20f9
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-03-05 11:05:17 -0800

    Complete renderForm function
    The renderForm function is now complete, it renders a label, input,
    select, and button as part of a form for submitting a location to get
    the weather of. It is not connected at all, this is just for creating
    the html and I added a bit of styling, mostly grid stuff for alignment.

--- src/render.js
+++ src/render.js
@@ -26,6 +26,34 @@ const renderForm = () => {
     const form = document.createElement("form");
     form.id = "place-form";
 
+    const searchLabel = document.createElement("label");
+    searchLabel.textContent = "Address, partial address, or lat,long coordinates";
+    searchLabel.setAttribute("for", "place-search");
+    const searchInput = document.createElement("input");
+    searchInput.id = "place-search";
+    const errorMessage = document.createElement("p");
+    errorMessage.classList.add("error");
+
+    const selectContainer = document.createElement("div");
+    selectContainer.classList.add("select-container");
+    const unitSelect = document.createElement("select");
+    const usOption = document.createElement("option");
+    usOption.text = "US";
+    usOption.value = "us";
+    const metricOption = document.createElement("option");
+    metricOption.text = "Metric";
+    metricOption.value = "metric";
+    unitSelect.append(usOption, metricOption);
+    selectContainer.append(unitSelect);
+
+    const btnContainer = document.createElement("div");
+    btnContainer.classList.add("btn-container");
+    const submitBtn = document.createElement("button");
+    submitBtn.classList.add("search");
+    submitBtn.textContent = "Search";
+    btnContainer.append(submitBtn);
+
+    form.append(searchLabel, searchInput, errorMessage, selectContainer, btnContainer);
 
     return form;
 }
